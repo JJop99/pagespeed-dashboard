90cf93544ec779fc30e198b281991a527327d334
marco-corrente <marco.corrente@mumbleideas.it>
2022-10-14 12:10:11 +0200

    Initial commit

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function test(): JsonResponse
+    {
+
+        return response()->json('test');
+    }
+}
---
+++ resources/js/components/Example.js
@@ -0,0 +1,25 @@
+import React, {useEffect} from 'react';
+import axios from 'axios'
+
+
+function Example() {
+    useEffect(() => {
+        axios.get('/api/test').then()
+    }, [])
+
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Example Component</div>
+
+                        <div className="card-body">I'm an example component!</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Example;
