b21b5b924ad45f0c5e899d8f08b2cf98bc955a6b
Jacopo Jop <jacopo.jop@mumbleideas.it>
2022-10-24 15:32:24 +0200

    feat:  dashboard url

--- resources/js/components/dashboard/Dasboard.js
+++ resources/js/components/dashboard/Dasboard.js
@@ -1,12 +1,16 @@
+import { Box, Container, Grid } from "@mui/material";
 import axios from "axios";
 import { useContext, useEffect } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import AuthContext from "../../store/auth-context";
+import { TasksProgress } from "./tasks-progress";
 
 const Dashboard = () => {
     const location = useLocation();
     console.log(location);
-    
+
+    let audits;
+
     axios.defaults.withCredentials = true;
     const authCtx = useContext(AuthContext);
     let search = {
@@ -19,7 +23,7 @@ const Dashboard = () => {
         axios
             .post(
                 `/api/dashboard`,
-                { search },
+                { url: location.state.url },
                 {
                     headers: {
                         // Overwrite Axios's automatically set Content-Type
@@ -37,14 +41,48 @@ const Dashboard = () => {
             })
             .then((data) => {
                 //console.log(data.data.urls);
-
-                console.log(data);
+                audits = data.data.lighthouseResult.audits;
+                console.log(audits);
             })
             .catch((err) => {
                 alert(err.message);
             });
     }, []);
 
-    return <div>{search.url}</div>;
+    
+    return (
+        <div>
+            <Box
+                component="main"
+                sx={{
+                    flexGrow: 1,
+                    py: 8,
+                }}
+            >
+                <Container maxWidth={false}>
+                    <Grid container spacing={3}>
+                        <Grid item lg={3} sm={6} xl={3} xs={12}>
+                            <TasksProgress id={'5'} title={''} description={''} score={''}/>
+                        </Grid>
+                        <Grid item xl={3} lg={3} sm={6} xs={12}>
+                            <TasksProgress />
+                        </Grid>
+                        <Grid item xl={3} lg={3} sm={6} xs={12}>
+                            <TasksProgress />
+                        </Grid>
+                        <Grid item xl={3} lg={3} sm={6} xs={12}>
+                            <TasksProgress sx={{ height: "100%" }} />
+                        </Grid>
+                        <Grid item xl={3} lg={3} sm={6} xs={12}>
+                            <TasksProgress />
+                        </Grid>
+                        <Grid item xl={3} lg={3} sm={6} xs={12}>
+                            <TasksProgress sx={{ height: "100%" }} />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
+        </div>
+    );
 };
 export default Dashboard;
