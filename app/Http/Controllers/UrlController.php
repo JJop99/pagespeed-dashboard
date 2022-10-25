568e74f8e277f7230761ef7b1047a127e8357cba
Matteo Saetti <matteo.saetti@mumbleideas.it>
2022-10-25 12:37:09 +0200

    fix(UrlController.php): fix dashboard

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -67,20 +67,34 @@ class UrlController extends Controller
         $validated = $request->validate([
             'id' => 'required'
         ]);
-        $statistics = URL::select('firstContentfulPaint', 'speedIndex', 'largestContentfulPaint','totalBlockingTime', 'cumulativeLayoutShift', 'interactive')
+        $firstContentfulPaint = URL::select('firstContentfulPaint')
         ->where('id', $validated['id'])
         ->get();
+        $speedIndex = URL::select('speedIndex')
+        ->where('id', $validated['id'])
+        ->get();
+        $largestContentfulPaint = URL::select('largestContentfulPaint')
+        ->where('id', $validated['id'])
+        ->get();
+        $totalBlockingTime = URL::select('totalBlockingTime')
+        ->where('id', $validated['id'])
+        ->get();
+        $cumulativeLayoutShift = URL::select('cumulativeLayoutShift')
+        ->where('id', $validated['id'])
+        ->get();
+        $interactive = URL::select('interactive')
+        ->where('id', $validated['id'])
+        ->get();
+        $statistics = [
+            $firstContentfulPaint[0],
+            $speedIndex[0],
+            $largestContentfulPaint[0],
+            $totalBlockingTime[0],
+            $cumulativeLayoutShift[0],
+            $interactive[0]
+        ];
         
-        //$statistics = [
-        //    $firstContentfulPaint[0],
-        //    $speedIndex[0],
-        //    $largestContentfulPaint[0],
-        //    $totalBlockingTime[0],
-        //    $cumulativeLayoutShift[0],
-        //    $interactive[0]
-        //];
- 
-        return response()->json($statistics);
+        return $statistics;
     }
     
     public function research(Request $request){
