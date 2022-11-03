99bd0235aff098f4efce01dfb8965f013c206b01
Matteo Saetti <matteo.saetti@mumbleideas.it>
2022-11-03 15:39:49 +0100

    fix: Relationship

--- app/Http/Controllers/AuditController.php
+++ app/Http/Controllers/AuditController.php
@@ -134,9 +134,10 @@ class AuditController extends Controller
 
         // $take = $request['take'] ?? '5';
         // $skip = $request['skip'] ?? '0';
-
-        $startDate = date('Y-m-d', strtotime($request->start_date)) ?? Carbon::now()->subDays(31)->format('Y-m-d');
-        $endDate =   date('Y-m-d', strtotime($request->end_date)) ?? Carbon::now()->format('Y-m-d');
+        $startDate = Carbon::now()->subDays(31)->format('Y-m-d');
+        $endDate = Carbon::now()->format('Y-m-d');
+        //$startDate = date('Y-m-d', strtotime($request->start_date)) ?? Carbon::now()->subDays(31)->format('Y-m-d');
+        //$endDate =   date('Y-m-d', strtotime($request->end_date)) ?? Carbon::now()->format('Y-m-d');
 
         $research = Audit::select('performance', 'created_at')
             ->where('email', Auth::user()->email)
